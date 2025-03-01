bf793913008f24d53fb3b592f178d0144e16f1b8
chikokone <chikatakone@icloud.com>
2025-02-28 21:15:51 -0500

    first commit

---
+++ LibraClouds/src/main/java/chikoKone/App.java
@@ -0,0 +1,13 @@
+package chikoKone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
